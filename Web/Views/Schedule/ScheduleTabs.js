e0d72b349e7ba0cb451ac8db33d4b5e68d03deca
Joey Fox <joefoxman@gmail.com>
2016-06-13 18:06:33 -0400

    Calendar Timeline data edits

--- Web/Views/Schedule/ScheduleTabs.js
+++ Web/Views/Schedule/ScheduleTabs.js
@@ -241,22 +241,22 @@
 
         scheduler.init("scheduler_here", new Date(2014, 5, 30), "timeline");
         scheduler.parse([
-			{ start_date: "2014-06-30 09:00", end_date: "2014-06-30 12:00", text: "Task A-12458", section_id: 20 },
-			{ start_date: "2014-06-30 10:00", end_date: "2014-06-30 16:00", text: "Task A-89411", section_id: 20 },
-			{ start_date: "2014-06-30 10:00", end_date: "2014-06-30 14:00", text: "Task A-64168", section_id: 20 },
-			{ start_date: "2014-06-30 16:00", end_date: "2014-06-30 17:00", text: "Task A-46598", section_id: 20 },
+			{ start_date: "2014-06-30 09:00", end_date: "2014-06-30 12:00", text: "Case A-12458", section_id: 20 },
+			{ start_date: "2014-06-30 10:00", end_date: "2014-06-30 16:00", text: "Case A-89411", section_id: 20 },
+			{ start_date: "2014-06-30 10:00", end_date: "2014-06-30 14:00", text: "Case A-64168", section_id: 20 },
+			{ start_date: "2014-06-30 16:00", end_date: "2014-06-30 17:00", text: "Case A-46598", section_id: 20 },
 
-			{ start_date: "2014-06-30 12:00", end_date: "2014-06-30 20:00", text: "Task B-48865", section_id: 40 },
-			{ start_date: "2014-06-30 14:00", end_date: "2014-06-30 16:00", text: "Task B-44864", section_id: 40 },
-			{ start_date: "2014-06-30 16:30", end_date: "2014-06-30 18:00", text: "Task B-46558", section_id: 40 },
-			{ start_date: "2014-06-30 18:30", end_date: "2014-06-30 20:00", text: "Task B-45564", section_id: 40 },
+			{ start_date: "2014-06-30 12:00", end_date: "2014-06-30 20:00", text: "Case B-48865", section_id: 40 },
+			{ start_date: "2014-06-30 14:00", end_date: "2014-06-30 16:00", text: "Case B-44864", section_id: 40 },
+			{ start_date: "2014-06-30 16:30", end_date: "2014-06-30 18:00", text: "Case B-46558", section_id: 40 },
+			{ start_date: "2014-06-30 18:30", end_date: "2014-06-30 20:00", text: "Case B-45564", section_id: 40 },
 
-			{ start_date: "2014-06-30 08:00", end_date: "2014-06-30 12:00", text: "Task C-32421", section_id: 60 },
-			{ start_date: "2014-06-30 14:30", end_date: "2014-06-30 16:45", text: "Task C-14244", section_id: 60 },
+			{ start_date: "2014-06-30 08:00", end_date: "2014-06-30 12:00", text: "Case C-32421", section_id: 60 },
+			{ start_date: "2014-06-30 14:30", end_date: "2014-06-30 16:45", text: "Case C-14244", section_id: 60 },
 
-			{ start_date: "2014-06-30 09:20", end_date: "2014-06-30 12:20", text: "Task D-52688", section_id: 80 },
-			{ start_date: "2014-06-30 11:40", end_date: "2014-06-30 16:30", text: "Task D-46588", section_id: 80 },
-			{ start_date: "2014-06-30 12:00", end_date: "2014-06-30 18:00", text: "Task D-12458", section_id: 80 }
+			{ start_date: "2014-06-30 09:20", end_date: "2014-06-30 12:20", text: "Case D-52688", section_id: 80 },
+			{ start_date: "2014-06-30 11:40", end_date: "2014-06-30 16:30", text: "Case D-46588", section_id: 80 },
+			{ start_date: "2014-06-30 12:00", end_date: "2014-06-30 18:00", text: "Case D-12458", section_id: 80 }
         ], "json");
     };
 
